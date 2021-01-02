88df1a39fa7c02bcc12fc95e4320edcb2c57228b
Yevgeny York <yy@ixi.ru>
2021-01-02 15:23:35 +0300

    Add lab6_1.R

---
+++ lab6_1.R
@@ -0,0 +1,20 @@
+library("devtools")
+#install_github("dgrtwo/broom")
+#install_github("cran/bstats")
+
+library("lubridate") # работа с датами
+
+library("sandwich") #vcovHC, vcovHAC
+library("lmtest") # tests
+library("car") # more tests
+library("bstats") # more more tests
+library("zoo") # временные ряды
+library("xts") # ещё ряды
+library("dplyr") # манипуляции с данными
+library("broom") # просто манипуляции
+library("ggplot2") # графики
+
+library("quantmod") # загрузка данных с finance.google.com
+library("rusquant") # загрузка данных с finam.ru
+library("sophisthse") # загрузка данных с sophist.hse.ru
+library("Quandl")
