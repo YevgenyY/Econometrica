f22e43042809a51ef66712a1139f69a3dea7b246
Yevgeny York <yy@ixi.ru>
2021-01-07 20:49:13 +0300

    Add ROC curve in lab6

--- lab7_1.R
+++ lab7_1.R
@@ -61,3 +61,47 @@ m_logit2 <- glm(data=t2, survived ~ sex +age, family=binomial(lin="logit"),x=TRU
 # Сравним две моделиб H0: они одинаковые
 # p-value ~0 - H0 - отвергаем
 lrtest(m_logit, m_logit2)
+
+# Оценка предельных эффектов
+# В logit-модели интерпретация коэффициентов - это отношение шансов
+
+# показать предельные эффекты для "среднего" пассажира
+# Marginal Effect for Binary Probit and Logit Model
+maBina(m_logit)
+
+# этот предельный эффект считает на сколько увеличение возраста 
+# на 1 год для каждого пассажира увеличивает его вероятность выживания 
+# а затем считает среднее
+
+# предельный эффект для каждого пассажира
+# считаем средний арифметическое по всем пассажирам
+
+# Например, в можели ниже, увеличение возраста на 1 год уменьшает вероятность 
+# выжить на 0.008, т.е. 0.8%
+# Для факторной переменной пол/sex - в модели ниже
+# Вероятность выжить для мужчины - вероятность выжить для женщины = 0.55 (процентов)
+maBina(m_logit, x.mean = TRUE) 
+
+# попробуем применить (неподходящий для этого) метод наименьших квадратов
+m_ols <- lm(data=t, as.numeric(survived) ~ sex +age + pclass +fare)
+summary(m_ols)
+pr_ols <- predict(m_ols,newdata)
+head(pr_ols) # видим вероятности больше 1
+
+# ROC-кривая
+# это прогноз для внутренней переменной
+# прежде чем интерпретировать, нужно применить к ней функцию логистического распределения
+pr_t <- predict(m_logit, t, se=TRUE) 
+t <- cbind(t, pr_t)
+t <- mutate(t,prob=plogis(fit))
+select(t, age, survived, prob)
+
+# Смотрим на пороги отсечения - ROC-кривая
+# cutoffs - перебор всех возможных порогов отсечения
+# fpr - false positive rate 
+# tpr - true positive rates
+roc.data <- roc(t$prob, t$survived)
+str(roc.data)
+qplot(x=roc.data$cutoffs, y=roc.data$tpr, geom="line")
+qplot(x=roc.data$cutoffs, y=roc.data$fpr, geom="line")
+qplot(x=roc.data$fpr, y=roc.data$tpr, geom="line")
