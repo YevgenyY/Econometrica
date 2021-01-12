05dfd1adec9f1a32160e8023c541f2fff80806cb
Yevgeny Y <yy@ixi.ru>
2021-01-12 16:05:37 +0300

    Add 10 exs from task 7

--- lab7_test.R
+++ lab7_test.R
@@ -17,3 +17,34 @@ left_ci <- E - z_crit * sqrt(var_theta_hat)
 right_ci <- E + z_crit * sqrt(var_theta_hat)
 left_ci
 right_ci
+
+# 4
+# y_hat = -1 + 0.25*x_i + 0.05*z_i
+# find x_i for max( dP(y_i = 1)/dz) if z_i = 10
+# используем правило вычисление производной сложной функции
+# dP(y_i=1) = dF(-1+0.25x_i+0.05z_i)/dz = f(-1+0.25x_i+0.05z_i)*0.05
+# Найти x_i при котором предельный эффект (производная) максимален
+u <- seq(from = -10, to=10, by=0.1)
+y <- exp(-u)/(1+exp(-u))^2
+qplot(u, y, geom="line")
+
+# u should be zero then
+# -1 + 0.25*x + 0.05*10 = 0 => x=2
+
+
+# 5
+# ln( f(x) )
+
+# 6
+# L(a) = nln(a) + (a-1)sum( ln(x) ) - an(ln(2)) 
+# mul(x_i) = 7.78 * 10^(-20)
+# calculate derivative by a and find extremum
+p <- 7.78 * ( 10^-20)
+n <- 100
+a_hat <- n / ( n*log(2) - log(p) )
+a_hat
+
+# 11
+require(graphics)
+## Full mosaic
+mosaicplot(HairEyeColor)
