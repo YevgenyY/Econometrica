9ad654168bdbca4821c0bf7bce830901de99b8c7
Yevgeny Y <yy@ixi.ru>
2021-01-14 12:33:28 +0300

    Finish week 7 test

--- lab7_test.R
+++ lab7_test.R
@@ -1,3 +1,9 @@
+library("dplyr")
+library("broom")
+library("mfx")
+library("vcd")
+library("erer")
+
 # 1 LR-stat
 2*(-300 + 310)
 qchisq(p=0.99, df=1)
@@ -51,3 +57,106 @@ a_hat
 require(graphics)
 ## Full mosaic
 mosaicplot(HairEyeColor)
+
+# 12
+options(stringsAsFactors = FALSE)
+
+t <- read.csv("data/titanic3.csv")
+glimpse(t)
+
+t <- mutate(t, sex=as.factor(sex), pclass=as.factor(pclass), 
+            survived=as.factor(survived))
+glimpse(t)
+summary(t)
+
+m_logit <- glm(data=t, survived ~ age + sex + fare + sibsp,
+               family=binomial(link="logit"), x=TRUE)
+summary(m_logit)
+
+# 13
+m_logit <- glm(data=t, survived ~ age + I(age^2) + sex + fare  + I(fare^2) + sibsp,
+               family=binomial(link="logit"), x=TRUE)
+summary(m_logit)
+b <- m_logit$coefficients[2] # age_i
+a <- m_logit$coefficients[3] # age_i^2
+-b/(2*a)
+
+# 14
+m_logit <- glm(data=t, survived ~ age + I(age^2) + sex + fare  + I(fare^2) + sibsp,
+               family=binomial(link="logit"), x=TRUE)
+summary(m_logit)
+confint(m_logit)
+
+# 15
+m_logit <- glm(data=t, survived ~ age + I(age^2) + sex + fare + sibsp,
+               family=binomial(link="logit"), x=TRUE)
+summary(m_logit)
+newdata <- data.frame(age=40, sex=as.factor("male"), fare=200, sibsp=2)
+pr <- predict(m_logit, newdata, se=TRUE)
+pr
+plogis(pr$fit)
+
+# 16
+m_logit <- glm(data=t, survived ~ age + I(age^2) + sex + fare + sibsp,
+               family=binomial(link="logit"), x=TRUE)
+summary(m_logit)
+newdata <- data.frame(age=50, sex=as.factor("male"), fare=200, sibsp=2)
+pr <- predict(m_logit, newdata, se=TRUE)
+pr
+ci_left <- pr$fit - 1.96*pr$se.fit
+plogis(ci_left)
+
+# 17
+m_logit <- glm(data=t, survived ~ age + I(age^2) + sex + fare + sibsp + parch,
+               family=binomial(link="logit"), x=TRUE)
+
+mr_eff <- maBina(m_logit, x.mean=FALSE)
+mr_eff
+round( mr_eff$out[1]$effect[6], 2 )
+logitmfx(data = t, survived ~ age + I(age^2) + fare + sibsp + parch)
+
+
+# 18
+d <- dplyr::select(t, age, sibsp, sex, fare, parch, survived) %>% na.omit()
+m_logit <- glm(data=d, survived ~ age + I(age^2) + 
+                 sex + fare + 
+                 I(fare^2) + sibsp + parch,
+               family=binomial(link="logit"), x=TRUE)
+m_logit2 <- glm(data=d, survived ~ age + I(age^2) + 
+                 sex + sibsp + parch,
+               family=binomial(link="logit"), x=TRUE)
+
+summary(m_logit2)
+lrtest(m_logit, m_logit2)
+
+# 19
+d <- dplyr::select(t, age, sibsp, sex, fare, parch, survived) %>% na.omit()
+m_logit <- glm(data=d, survived ~ age + I(age^2) + 
+                 sex + fare + sibsp, family=binomial(link="logit"), x=TRUE)
+summary(m_logit)
+
+newdata <- d
+pr_logit <- predict(m_logit, newdata, se=TRUE)
+newdata_pr <- cbind(newdata, pr_logit)
+head(newdata_pr)
+
+newdata_pr <- mutate(newdata_pr, prob=plogis(fit), 
+                     left_ci = plogis(fit-1.96*se.fit),
+                     right_ci= plogis(fit+1.96*se.fit))
+head(newdata_pr)
+h <- dplyr::select(newdata_pr, survived, prob)
+h
+roc.data <- roc(h$prob, h$survived)
+qplot(roc.data$cutoffs, y=roc.data$tpr)
+qplot(roc.data$cutoffs, y=roc.data$fpr)
+qplot(roc.data$fpr, y=roc.data$tpr)
+
+cutoff <- 0.65
+sv <- subset(h, prob >= 0.65)
+di <- subset(h, prob < 0.65)
+sum(sv$survived==1) / sum(d$survived==1) 
+
+# 20
+m_logit <- glm(data=t, survived ~ age + I(age^2) + sex + fare + sibsp,
+               family=binomial(link="logit"), x=TRUE)
+summary(m_logit)
